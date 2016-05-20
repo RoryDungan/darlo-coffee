4499aee2f9624e44c522fc456b96a98ffa3d8d58
Rory Dungan <rorydungan@gmail.com>
2016-05-20 23:38:09 +1000

    Moved config into its own file 📓

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 var request = require('request-promise');
 var cheerio = require('cheerio');
 var nodemailer = require('nodemailer');
+var yaml = require('js-yaml');
+var fs = require('fs');
 
 var url = 'https://www.darlocoffee.com/store/p8/Cold_Brew_in_a_Box_%282_Pack%29.html';
 var recipients = ['rorydungan@gmail.com'];
 
-var sendEmail = function() {
+var sendEmail = function(config) {
     var transport = nodemailer.createTransport({
 		service: "Gmail",
 		auth: {
-			user: "ohdamndarlocoffeefukyeah@gmail.com",
-			pass: "",
+			user: config.user,
+			pass: config.pass,
 		},
 	});
+    
+    if (!config.recipients || !(config.recipients instanceof Array)) {
+        console.error("No email recipients set up in config");
+        return;
+    }
     
     recipients.forEach(function (recipient) {
         
@@ -38,21 +45,39 @@ var sendEmail = function() {
     });
 };
 
-request(url)
-    .then(function (html) {
-
-        var $ = cheerio.load(html);
-
-        var quantityInput = $('#wsite-com-product-quantity-input');
-        var isOutOfStock = quantityInput.hasClass('wsite-com-product-disabled');
-        if (!isOutOfStock) {
-            // send email
-            sendEmail();
-            
-            console.log("Not out of stock any more!");
-        }
-        else {
-            console.log("Still out of stock");
-        }
-    });
+// 
+// Get config
+//
+try {
+    var config = yaml.safeLoad(fs.readFileSync('config.yaml', 'utf-8'));
     
+    console.log(config);
+    
+    //
+    // Execute request
+    //
+    request(url)
+        .then(function (html) {
+
+            var $ = cheerio.load(html);
+
+            var quantityInput = $('#wsite-com-product-quantity-input');
+            var isOutOfStock = quantityInput.hasClass('wsite-com-product-disabled');
+            if (!isOutOfStock) {
+                // send email
+                sendEmail(config);
+                
+                console.log("Not out of stock any more!");
+            }
+            else {
+                console.log("Still out of stock");
+            }
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+        
+}
+catch (e) {
+    console.error(e);
+}
