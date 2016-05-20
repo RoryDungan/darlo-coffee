66b8a3d4ba2dc1a2f2252796737369d4f5183595
Rory Dungan <rorydungan@gmail.com>
2016-05-20 21:35:59 +1000

    Added mailer

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 var request = require('request-promise');
 var cheerio = require('cheerio');
+var nodemailer = require('nodemailer');
 
-request('https://www.darlocoffee.com/store/p8/Cold_Brew_in_a_Box_%282_Pack%29.html')
+var url = 'https://www.darlocoffee.com/store/p8/Cold_Brew_in_a_Box_%282_Pack%29.html';
+var recipients = ['rorydungan@gmail.com'];
+
+var sendEmail = function() {
+    var transport = nodemailer.createTransport({
+		service: "Gmail",
+		auth: {
+			user: "ohdamndarlocoffeefukyeah@gmail.com",
+			pass: "",
+		},
+	});
+    
+    recipients.forEach(function (recipient) {
+        
+        transport.sendMail(
+            {
+                to: recipient,
+                from: 'ohdamndarlocoffeefukyeah@gmail.com',
+                replyTo: 'ohdamndarlocoffeefukyeah@gmail.com',
+                subject: 'Darlo coffee available for order again ☕️',
+                text: "Hell yeah " + url,
+                attachments: [],
+            }, 
+            function (err, response) {
+
+                if (err) {
+                    console.error('Error occured sending email', err);
+                }
+                else {
+                    console.log("Message sent: ");
+                    console.log(response);
+                }
+            });
+    });
+};
+
+request(url)
     .then(function (html) {
 
         var $ = cheerio.load(html);
@@ -9,7 +46,8 @@ request('https://www.darlocoffee.com/store/p8/Cold_Brew_in_a_Box_%282_Pack%29.ht
         var quantityInput = $('#wsite-com-product-quantity-input');
         var isOutOfStock = quantityInput.hasClass('wsite-com-product-disabled');
         if (!isOutOfStock) {
-            // send email.
+            // send email
+            sendEmail();
             
             console.log("Not out of stock any more!");
         }
@@ -17,3 +55,4 @@ request('https://www.darlocoffee.com/store/p8/Cold_Brew_in_a_Box_%282_Pack%29.ht
             console.log("Still out of stock");
         }
     });
+    
